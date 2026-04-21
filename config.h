2c4456287bf1c0497d306b12c9e2893f40cf9a02
ggienio <genini98@gmail.com>
2026-04-21 21:43:10 +0200

    implement pwm motor control

--- config.h
+++ config.h
@@ -8,17 +8,17 @@ namespace Pins {
     //  |   |
     // M3---M4
     //
-    constexpr int MOTOR1_PIN_A = 0;
-    constexpr int MOTOR1_PIN_B = 0;
+    constexpr int MOTOR1_PIN_A = 1;
+    constexpr int MOTOR1_PIN_B = 2;
     
-    constexpr int MOTOR2_PIN_A = 0;
-    constexpr int MOTOR2_PIN_B = 0;
+    constexpr int MOTOR2_PIN_A = 42;
+    constexpr int MOTOR2_PIN_B = 41;
     
-    constexpr int MOTOR3_PIN_A = 0;
-    constexpr int MOTOR3_PIN_B = 0;
+    constexpr int MOTOR3_PIN_A = 40;
+    constexpr int MOTOR3_PIN_B = 39;
 
-    constexpr int MOTOR4_PIN_A = 0;
-    constexpr int MOTOR4_PIN_B = 0;
+    constexpr int MOTOR4_PIN_A = 47;
+    constexpr int MOTOR4_PIN_B = 21;
 }
 
 namespace WiFiConf {
@@ -28,6 +28,7 @@ namespace WiFiConf {
 
 namespace Motors {
     constexpr int PWM_RESOLUTION = 12; // 12 bit resolution: 0 - 4095
+    constexpr int PWM_FREQUENCY = 5000;
     constexpr int SPEED_CAP = 2047; // should not exceed 65% as the motors are rated for 9V, driver 3A continuous
 }
 
--- src/motors/motor.cpp
+++ src/motors/motor.cpp
@@ -5,13 +5,32 @@
 Motor::Motor(int pin_a, int pin_b) {
     this->pin_a = pin_a;
     this->pin_b = pin_b;
+
+    ledcAttach(this->pin_a, Motors::PWM_FREQUENCY, Motors::PWM_RESOLUTION);
+    ledcAttach(this->pin_b, Motors::PWM_FREQUENCY, Motors::PWM_RESOLUTION);
+
     this->set_speed(0);
 }
 
 void Motor::set_speed(int speed) {
-    Serial.println("Speed set");
+    // Constrain values exceeding the limit from config.h
     this->speed = constrain(speed, -Motors::SPEED_CAP, Motors::SPEED_CAP);
-    // TODO: implement
-    // set pin_a (or b - if speed < 0) to a pwm signal of value this->speed, the other one to 0
-    // use the ledc library
+
+    // Current control logic for the H-bridge (Cytron MDD3A)
+    if (this->speed > 0) {
+        // Forward direction - apply PWM to pin A, set pin B to zero
+        ledcWrite(this->pin_a, this->speed);
+        ledcWrite(this->pin_b, 0);
+
+    } else if (this->speed < 0) {
+        // Backward direction - set pin A to zero, apply PWM to pin B
+        // ledcWrite only accepts positive numbers
+        ledcWrite(this->pin_a, 0);
+        ledcWrite(this->pin_b, abs(this->speed));
+
+    } else {
+        // Stop (speed 0) - turn off power on both pins
+        ledcWrite(this->pin_a, 0);
+        ledcWrite(this->pin_b, 0);
+    }
 }
